6e0e964556ccd16acd3117305d4b17dea5e4d5df
Roka <roka15941@gmail.com>
2025-08-09 17:57:07 +0300

    Create task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+ using namespace std;
+ #define ll long long
+ int main(){
+ int num;
+ do{
+  cout<<"enter a  positive number(to stop the loop enter a negative number): ";
+  cin>>num;
+ }
+ while(num>0);
+   return 0;
+   
+ }
